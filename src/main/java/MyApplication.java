6615dd90d7d7a07884437c61c66350a0c975a6bb
Thiago Born <thiagofborn@gmail.com>
2022-03-16 23:19:35 +0000

    test file

---
+++ src/main/java/MyApplication.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+
+@RestController
+@SpringBootApplication
+public class MyApplication {
+
+    @RequestMapping("/api")
+    String home() {
+        return "Hello World Spring Boot - Using @RequestMapping /api";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
